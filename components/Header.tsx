416d150e93f79057d5b977e9076646e7b2890398
viisualworks <dshege4@gmail.com>
2025-03-05 15:49:14 +0300

    display a specific image next to each heading

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,22 +4,32 @@ import { usePathname } from "next/navigation";
 import Image from "next/image";
 import moonIcon from "@/public/icon-moon-dark.svg";
 import sunIcon from "@/public/icon-sun-dark.svg";
+import accessibilityIcon from "@/public/icon-accessibility.svg"
+
 
 export default function Header() {
     const pathname = usePathname(); // Get the current route
 
-    const pageTitles: {[key:string]: string} = {
-        "/" : "",
-        "/accessibility": "Accessibility",
-        "/html": "HTML",
+    // Define titles and images based on the route
+    const pageData: {[key:string]: { title: string; icon: string }} = {
+        "/" : {title: "", icon: ""},
+        "/accessibility": {title: "Accessibility", icon: accessibilityIcon },
     }
 
-    // Set the heading based on the pathname, default to "Page"
-    const heading = pageTitles[pathname] || ""
+    const pagexData = {
+        "/": {title: "", icon:""},
+        "/accessibility": {title: "", icon:""}
+    }
+
+    // Set the heading based on the pathname, default to ""
+    const { title, icon } = pageData[pathname] || {title: "", icon: ""}
 
   return (
     <header className="flex justify-between pb-20">
-      <h1 className="text-2xl font-semibold text-dark-navy">{heading}</h1>
+        <div className="flex items-center gap-x-4">
+            <Image className="p-2 bg-fuchsia-100 rounded-md" src={icon} alt={`${title} icon`} />
+            <h1 className="text-2xl font-semibold text-dark-navy">{title}</h1>
+        </div>
       <div className="flex items-center gap-x-2">
         <Image src={sunIcon} alt="the sun icon" />
         <label
