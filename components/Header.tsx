f95d072cbd6d925dc26f28a10b2208a1a1bc93b9
viisualworks <dshege4@gmail.com>
2025-03-05 13:58:24 +0300

    use usePathname to dynamically change the headings in my header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,25 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import Image from "next/image";
 import moonIcon from "@/public/icon-moon-dark.svg";
 import sunIcon from "@/public/icon-sun-dark.svg";
 
 export default function Header() {
+    const pathname = usePathname(); // Get the current route
+
+    const pageTitles: {[key:string]: string} = {
+        "/" : "",
+        "/accessibility": "Accessibility",
+        "/html": "HTML",
+    }
+
+    // Set the heading based on the pathname, default to "Page"
+    const heading = pageTitles[pathname] || ""
+
   return (
     <header className="flex justify-between pb-20">
-      <h1 className="text-2xl font-semibold text-dark-navy">Accessibility</h1>
+      <h1 className="text-2xl font-semibold text-dark-navy">{heading}</h1>
       <div className="flex items-center gap-x-2">
         <Image src={sunIcon} alt="the sun icon" />
         <label
