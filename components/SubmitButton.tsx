40edb1818f532bd05526a82f7270d9b1316781ca
viisualworks <dshege4@gmail.com>
2025-03-07 21:22:17 +0300

    add submit answer logic to the button

--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -1,5 +1,19 @@
-export default function SubmitButton(){
-    return (
-        <button className="hover:bg-fuchsia-400 bg-purple cursor-pointer p-4 rounded-xl text-white text-lg"> Submit Answer </button>
-    )
+"use client";
+
+import { useState } from "react";
+
+export default function SubmitButton() {
+  const [submit, setSubmit] = useState<String>("Submit Answer");
+
+  const submitAnswer = () => {
+    setSubmit("Next Question");
+  }
+  
+  return (
+    <button
+      className="hover:bg-fuchsia-400 bg-purple cursor-pointer p-4 rounded-xl text-white text-lg"
+      onClick={submitAnswer}>
+      {submit}
+    </button>
+  );
 }
