c28051687ddd129e75bfe495a1504e32c013e04c
viisualworks <dshege4@gmail.com>
2025-03-08 00:06:38 +0300

    add an error icon to the error message

--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -1,19 +1,31 @@
+import Image from "next/image";
+import errorIcon from "@/public/icon-error.svg";
+
 interface SubmitButtonProps {
-    submitText: string;
-    submitAnswer: () => void;
-    errorMessage: string | null;
+  submitText: string;
+  submitAnswer: () => void;
+  errorMessage: string | null;
 }
 
-export default function SubmitButton ({submitText, submitAnswer, errorMessage} : SubmitButtonProps) {
+export default function SubmitButton({
+  submitText,
+  submitAnswer,
+  errorMessage,
+}: SubmitButtonProps) {
   return (
     <div>
-        <button
-          className="hover:bg-fuchsia-400 w-full bg-purple cursor-pointer p-4 rounded-xl text-white text-lg"
-          onClick={submitAnswer}>
-          {submitText}
-        </button>
+      <button
+        className="hover:bg-fuchsia-400 w-full bg-purple cursor-pointer p-4 rounded-xl text-white text-lg"
+        onClick={submitAnswer}>
+        {submitText}
+      </button>
 
-        { errorMessage && <p className="text-red mt-2">{errorMessage}</p> }
+      {errorMessage && (
+        <p className="text-red mt-4 flex justify-center items-center gap-2">
+            <Image src={errorIcon} alt="the error icon"></Image>
+          {errorMessage}
+        </p>
+      )}
     </div>
-  )
+  );
 }
