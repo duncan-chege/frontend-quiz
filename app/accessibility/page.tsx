b3869c7d8f364d4aa165593a9432916be88cb5ff
viisualworks <dshege4@gmail.com>
2025-03-07 23:54:06 +0300

    lift the state up from the accessibility component to submit button component

--- app/accessibility/page.tsx
+++ app/accessibility/page.tsx
@@ -7,8 +7,11 @@ import { useState } from "react";
 
 export default function AccessibilityPage() {
   const [selectedOption, setSelectedOption] = useState<number | null>(null);
-  const questions = quizData.quizzes[3].questions; // Selects the 4th object in the array- The Accessibility quiz. It then extracts the questions array from that quiz
   const [currentIndex, setCurrentIndex] = useState<number>(0); // It starts at the first question.
+  const [submitText, setSubmitText] = useState<string>("Submit Answer");
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  const questions = quizData.quizzes[3].questions; // Selects the 4th object in the array- The Accessibility quiz. It then extracts the questions array from that quiz
 
   // questions is the full array of questions.
   // questions[currentIndex] picks one question based on currentIndex.
@@ -16,17 +19,36 @@ export default function AccessibilityPage() {
 
   const optionLabels = ["A", "B", "C", "D"];
 
+  const handleOptionSelect = (option: number) => {
+    setSelectedOption(option);
+  };
+
+  const submitAnswer = () => {
+    if (selectedOption === null) {
+      setErrorMessage("Please select an answer");
+      return; // Stop execution here if no option is selected
+    }
+
+    setSubmitText("Next Question");
+    setErrorMessage("");
+  };
+
   return (
     <div
       className="grid grid-cols-1 lg:grid-cols-2 gap-x-32 gap-y-8"
       role="main">
       <div>
-        <p className="italic text-grey-navy">Question {currentIndex + 1} out of {questions.length}</p>
+        <p className="italic text-grey-navy">
+          Question {currentIndex + 1} out of {questions.length}
+        </p>
         <h3 className="mt-8 font-bold text-2xl md:text-3xl text-dark-navy">
           {currentQuestion.question}
         </h3>
 
-        <ProgressBar currentIndex={currentIndex} totalQuestions={questions.length} />
+        <ProgressBar
+          currentIndex={currentIndex}
+          totalQuestions={questions.length}
+        />
       </div>
       <div className="flex flex-col gap-y-6">
         {currentQuestion.options.map((option, index) => (
@@ -38,7 +60,7 @@ export default function AccessibilityPage() {
                   ? "outline outline-[3px] outline-purple"
                   : ""
               }`}
-            onClick={() => setSelectedOption(index)}>
+            onClick={() => handleOptionSelect(index)}>
             <span
               className={`group-hover:bg-fuchsia-100 group-hover:text-purple bg-light-grey text-grey-navy py-2 px-4 rounded-md text-2xl font-bold ${
                 selectedOption === index ? "bg-purple text-white" : ""
@@ -49,7 +71,11 @@ export default function AccessibilityPage() {
           </li>
         ))}
 
-        <SubmitButton />
+        <SubmitButton
+          submitText={submitText}
+          submitAnswer={submitAnswer}
+          errorMessage={errorMessage}
+        />
       </div>
     </div>
   );
--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -1,19 +1,19 @@
-"use client";
+interface SubmitButtonProps {
+    submitText: string;
+    submitAnswer: () => void;
+    errorMessage: string | null;
+}
 
-import { useState } from "react";
-
-export default function SubmitButton() {
-  const [submit, setSubmit] = useState<String>("Submit Answer");
-
-  const submitAnswer = () => {
-    setSubmit("Next Question");
-  }
-  
+export default function SubmitButton ({submitText, submitAnswer, errorMessage} : SubmitButtonProps) {
   return (
-    <button
-      className="hover:bg-fuchsia-400 bg-purple cursor-pointer p-4 rounded-xl text-white text-lg"
-      onClick={submitAnswer}>
-      {submit}
-    </button>
-  );
+    <div>
+        <button
+          className="hover:bg-fuchsia-400 w-full bg-purple cursor-pointer p-4 rounded-xl text-white text-lg"
+          onClick={submitAnswer}>
+          {submitText}
+        </button>
+
+        { errorMessage && <p className="text-red mt-2">{errorMessage}</p> }
+    </div>
+  )
 }
