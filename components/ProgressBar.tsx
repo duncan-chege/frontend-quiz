530e4b2c8bba6f24f6ff8c8dd3e07b00fe65c3fd
viisualworks <dshege4@gmail.com>
2025-03-26 15:00:31 +0300

    reduce the margin top progress bar space

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -7,7 +7,7 @@ const ProgressBar: React.FC<ProgressBarProps> = ({currentIndex, totalQuestions})
     const progress = ((currentIndex + 1)/totalQuestions) * 100;
 
     return(
-        <div className="w-full dark:bg-navy bg-white shadow-lg rounded-full h-4 mb-4 mt-32">
+        <div className="w-full dark:bg-navy bg-white shadow-lg rounded-full h-4 mb-4 mt-10 lg:mt-32">
             <div className="bg-purple h-4 rounded-full transition-all duration-300" style={{ width: `${progress}%` }}>
         </div>
     </div>
