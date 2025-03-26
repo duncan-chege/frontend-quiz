3994c58e160df0c896eccac9d17baf0fc625b5b6
viisualworks <dshege4@gmail.com>
2025-03-26 13:26:13 +0300

    add background dark mode images

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function HomePage() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-32" role="main">
       <div>
-        <h1 className="text-5xl font-light text-dark-navy">
+        <h1 className="dark:text-white text-5xl font-light text-dark-navy">
           Welcome to the <span className="font-bold">Frontend Quiz!</span>
         </h1>
         <p className="mt-8 mb-4 text-grey-navy italic">
